0baf6f41693cd657ed1cda86183f46a997c0642f
johngeorge88 <johniskandar@hotmail.com>
2020-08-22 22:30:58 -0400

    assigning variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
 // Assignment code here
+var password = {
+  // lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  lowerCase: ("abcdefghijklmnopqrstuvwxyz"),
+  upperCase: ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+  number: ("0123456789"),
+  symbol: ("!@#$%^&*()_-+={[}];'<,>.?/")
+}
 
+
+function getRandomLower() {
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
