783c2658b249a1944690dfe9a95593ec8fc47ff8
johngeorge88 <johniskandar@hotmail.com>
2020-08-23 09:58:24 -0400

    Password Length

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,40 @@ var password = {
   symbol: ("!@#$%^&*()_-+={[}];'<,>.?/")
 }
 
+//Password length
+var passwordLength = function() {
+  // ask about lenght
+  var passwordLengthPrompt = window.prompt(
+    "Please choose a length of at least 8 characters and no more than 128 characters"
+  );
+
+  // check if prompt answer was left blank, user hit "cancel", or provided a number instead
+  if (passwordLengthPrompt === null || passwordLengthPrompt === "" || isNaN(passwordLengthPrompt) || passwordLengthPrompt < 8 || passwordLengthPrompt > 128) {
+    window.alert("You need to provide a valid length! Please try again.");
+    return passwordLength();
+  }else {
+  // convert answer from prompt to an actual number
+  passwordLengthPrompt = parseInt(passwordLengthPrompt);
+  }
+  console.log(passwordLengthPrompt)
+}
+
+//Password Lower case
+var passwordLowerCase = function() {
+  //ask about lower Case
+  var passwordLowerPrompt = window.prompt(
+    "Would you like the password to have lower case letters? Enter 'YES' or 'NO' to choose."
+  );
+
+  // check if prompt answer was left blank, user hit "cancel", or provided a number instead
+  if (passwordLowerPrompt === "YES" || passwordLowerPrompt === "yes") {
+  return true;
+}else (passwordLowerPrompt === null || passwordLowerPrompt === "" || !isNaN(passwordLowerPrompt))
+  window.alert("You need to provide a valid length! Please try again.");
+  return passwordLowerCase();
+
+  console.log(passwordLowerCase)
+}
 
 function getRandomLower() {
 
@@ -26,3 +60,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+passwordLength ()
